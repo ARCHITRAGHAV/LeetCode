d64018821cd967146b10009bd8fe8c7075b027de
ARCHIT RAGHAV <121053866+ARCHITRAGHAV@users.noreply.github.com>
2025-07-18 05:15:46 +0530

    Time: 822 ms (9.82%) | Memory: 45.6 MB (16.03%) - LeetSync

---
+++ 647-palindromic-substrings/palindromic-substrings.java
@@ -0,0 +1,24 @@
+class Solution {
+
+    public static boolean checkPalindrome(String s) {
+        int i = 0, j = s.length() - 1;
+        while (i < j) {
+            if (s.charAt(i) != s.charAt(j))
+                return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+    public int countSubstrings(String s) {
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = i + 1; j <= s.length(); j++) {
+                if (checkPalindrome(s.substring(i, j)))
+                    count++;
+            }
+        }
+        return count;
+    }
+}
