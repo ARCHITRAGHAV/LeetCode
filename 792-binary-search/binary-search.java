797472fc5feb5a661c064ccfd948baed98a2e767
ARCHIT RAGHAV <121053866+ARCHITRAGHAV@users.noreply.github.com>
2025-07-18 05:19:48 +0530

    Time: 0 ms (100.00%) | Memory: 45.6 MB (89.54%) - LeetSync

---
+++ 792-binary-search/binary-search.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int search(int[] arr, int k) {
+        int start = 0;
+        int end = arr.length - 1;
+        while(start<=end){
+            int mid = (start+end)/2;
+            if(arr[mid]==k){
+                return mid;
+            } else if(arr[mid]>k){
+                end = mid - 1;
+            } else{
+                start = mid + 1;
+            }
+        }
+        return -1;
+
+    }
+}
