fd2b8fddeead3e55898c2d8e24cde30e447a349b
ARCHIT RAGHAV <121053866+ARCHITRAGHAV@users.noreply.github.com>
2025-07-18 06:18:30 +0530

    Time: 7 ms (48.97%) | Memory: 56.7 MB (62.53%) - LeetSync

--- 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.java
+++ 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.java
@@ -19,7 +19,7 @@ class Solution {
         }
         for (i = 0; i < arr.length; i++) {
             if (arr[i] != i + 1)
-                ans.add(i+1);
+                ans.add(i + 1);
         }
         return ans;
     }
