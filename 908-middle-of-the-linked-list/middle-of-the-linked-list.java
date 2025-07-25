85364ef3860942547aab907a12b99897b41a2615
ARCHIT RAGHAV <121053866+ARCHITRAGHAV@users.noreply.github.com>
2025-07-25 15:29:25 +0530

    Time: 0 ms (100.00%) | Memory: 41.6 MB (13.43%) - LeetSync

---
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.java
@@ -0,0 +1,15 @@
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode temp = head;
+        int size = 0;
+        while(temp!=null){
+            temp = temp.next;
+            size++;
+        }
+        temp = head;
+        for(int i = 0;i<size/2;i++){
+            temp = temp.next;
+        }
+        return temp;
+    }
+}
